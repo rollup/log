e8c324d305a832d3ccfe1bc158892df06e493063
shellscape <andrew@shellscape.org>
2018-07-18 00:08:00 -0400

    docs: add example script

---
+++ docs/example.js
@@ -0,0 +1,22 @@
+const chalk = require('chalk');
+
+const { logger } = require('../dist/index');
+
+const log = logger({ level: 'debug' });
+const timeLog = logger({ timestamp: true });
+
+console.log();
+
+log.info('batman');
+log.warn('aliens');
+log.error('joker');
+log.debug('batcave');
+
+console.log();
+
+timeLog.info('time to wash the batmobile')
+
+console.log();
+
+log.fail(chalk`{red mansion blowed up}`);
+log.pass(chalk`batcave is {green {bold clean}}`);
