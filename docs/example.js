c738b7c212387fbe3137a7514aaefc40b99db4f1
Andrew Powell <shellscape@users.noreply.github.com>
2018-08-10 12:09:23 -0400

    chore: update meta for publish (#2)

--- docs/example.js
+++ docs/example.js
@@ -3,21 +3,22 @@ const chalk = require('chalk');
 // eslint-disable-next-line import/no-unresolved
 const { logger } = require('../dist/index');
 
+const { log: line } = console;
 const log = logger({ level: 'debug' });
 const timeLog = logger({ timestamp: true });
 
-console.log();
+line();
 
 log.info('batman');
 log.warn('aliens');
 log.error('joker');
 log.debug('batcave');
 
-console.log();
+line();
 
 timeLog.info('time to wash the batmobile');
 
-console.log();
+line();
 
 log.fail(chalk`{red mansion blowed up}`);
 log.pass(chalk`batcave is {green {bold clean}}`);
